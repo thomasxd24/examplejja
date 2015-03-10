429cec76105525c68a0b5e8d711f69c9ac14e02b
thomasxd24 <tomas_tse24@me.com>
2015-03-10 08:07:45 +0100

    added project

---
+++ testing/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace testing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(289, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Faire les commmand avec les code (Excel obligataire)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 73);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(676, 80);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(311, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(316, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Rechercher les produit qui ne peuvent pas etre commandé";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Copiez les code au developper tool (F12)";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 159);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(676, 616);
+            this.webBrowser1.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(423, 42);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(311, 31);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 787);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ testing/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using mshtml;
+
+namespace testing
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            webBrowser1.Navigate("http://easy-rea.com/products/index");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                openFileDialog1.ShowDialog();
+                string path = openFileDialog1.FileName;
+                DataTable dt = exceldata(path);
+                richTextBox1.Text = "// ca commence..." + Environment.NewLine;
+                foreach (DataRow row in dt.Rows)
+                {
+                    string text = row[0].ToString();
+                    string textcap = text.ToUpper();
+                    richTextBox1.Text += "setTimeout(function(){updateProduct('" + text + "', '0', '/products/update', 1, 110, 'divAlertQteDispoMessage', 0, 900, 'divAlertNbRowsMessage'); }, 3000);" + Environment.NewLine;
+                }
+                button2.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Vous ete en train de utiliser le file excel, il faut que vous fermez. Oringinale erreur:" + ex);
+            }
+        }
+
+        public static DataTable exceldata(string filePath)
+        {     
+            DataTable dtexcel = new DataTable();
+                string HDR = "No";
+                string strConn;
+                if (filePath.Substring(filePath.LastIndexOf('.')).ToLower() == ".xlsx")
+                    strConn = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + filePath + ";Extended Properties=\"Excel 12.0;HDR=" + HDR + ";IMEX=0\"";
+                else
+                    strConn = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + filePath + ";Extended Properties=\"Excel 8.0;HDR=" + HDR + ";IMEX=0\"";
+                OleDbConnection conn = new OleDbConnection(strConn);
+                conn.Open();
+                DataTable schemaTable = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new object[] { null, null, null, "TABLE" });
+                //Looping Total Sheet of Xl File
+                /*foreach (DataRow schemaRow in schemaTable.Rows)
+                {
+                }*/
+                //Looping a first Sheet of Xl File
+                DataRow schemaRow = schemaTable.Rows[0];
+                string sheet = schemaRow["TABLE_NAME"].ToString();
+                if (!sheet.EndsWith("_"))
+                {
+                    string query = "SELECT  * FROM [sheet1$]";
+                    OleDbDataAdapter daexcel = new OleDbDataAdapter(query, conn);
+                    daexcel.Fill(dtexcel);
+                }
+            
+            conn.Close();
+            return dtexcel;
+ 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            string path = openFileDialog1.FileName;
+            DataTable dt = exceldata(path);
+            richTextBox1.Text = "var myElem = 'Les articles:'" + Environment.NewLine;
+            foreach (DataRow row in dt.Rows)
+            {
+                string text = row[0].ToString();
+                string textcap = text.ToUpper();
+                richTextBox1.Text += "if (document.getElementById('product" + textcap + "') == null) { myElem += '" + text + ",'; };" + Environment.NewLine;
+
+            }
+            richTextBox1.Text += "document.getElementById('footer').innerHTML = myElem + '. Ne peuvent pas etre commandé'";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            MyInvokeScript("updateProduct", "102550","4","/products/update",1,110,"divAlertQteDispoMessage",0,900,"divAlertNbRowsMessage");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+HtmlDocument doc = webBrowser1.Document;
+HtmlElement head = doc.GetElementsByTagName("head")[0];
+HtmlElement s = doc.CreateElement("script");
+s.SetAttribute("text","function sayHello() {updateProduct('709663341','0','/products/update',1,110,'divAlertQteDispoMessage',0,900,'divAlertNbRowsMessage'); }");
+head.AppendChild(s);
+        }
+
+        private object MyInvokeScript(string name, params object[] args)
+        {
+            return webBrowser1.Document.InvokeScript(name, args);
+        }
+    }
+}
